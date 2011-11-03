220abc8c9879e6a3c185016307702eb8990b1027
Luke Melia <luke@lukemelia.com>
2011-11-03 18:28:39 -0400

    Force the host header sent to S3 to have the Host header set as s3.amazonaws.com

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var http = require('http'),
     port = process.env.PORT || 8000;
 httpProxy.createServer(function (req, res, proxy) {
   res.setHeader('Access-Control-Allow-Origin', process.env.YAPP_S3PROXY_CORS_ALLOW_ORIGIN);
+  req.headers['Host'] = 's3.amazonaws.com';
   proxy.proxyRequest(req, res, {
     host: 's3.amazonaws.com',
     port: 80
