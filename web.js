e941ceca3e5a67baa6dcf8cd2017a0770bc5bb7e
Luke Melia <luke@lukemelia.com>
2011-11-06 20:08:00 -0500

    Allow any origin

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var http = require('http'),
     httpProxy = require('http-proxy');
     port = process.env.PORT || 8000;
 httpProxy.createServer(function (req, res, proxy) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.YAPP_S3PROXY_CORS_ALLOW_ORIGIN);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   req.headers['Host'] = 's3.amazonaws.com';
   proxy.proxyRequest(req, res, {
     host: 's3.amazonaws.com',
